2179e609f4f086eec3a0d6b0d83fe960c59cfa9f
clement <292621@supinfo.com>
2019-06-03 14:36:56 +0200

    Log

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/ClementTeyssa/3PJT-API3/config"
 )
 
 func main() {
+	config.DatabaseInit()
+	log.Println("Database initialised")
 	router := InitializeRouter()
-
+	log.Println("Rooter initialised")
 	log.Panic(http.ListenAndServe(":8080", router))
 }
