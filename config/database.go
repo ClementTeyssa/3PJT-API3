80bfb06fc578e856ad0c071900868b27c68d4b2e
clement <292621@supinfo.com>
2019-06-03 14:22:51 +0200

    database

--- config/database.go
+++ config/database.go
@@ -1,10 +1,21 @@
 package config
 
-import "database/sql"
+import (
+	"database/sql"
+	"log"
+)
 
 var db *sql.DB
 
 func DatabaseInit() {
+	var err error
+	//TODO: set var by .env
+	db, err = sql.Open("postgres", "user=test dbname=goapi password=test host=postgres port=5433 sslmode=disable")
+	// db, err = sql.Open("postgres", "user=test dbname=goapi password=test host=localhost port=5432 sslmode=disable")
+
+	if err != nil {
+		log.Panic(err)
+	}
 
 	createToRewardBase()
 }
